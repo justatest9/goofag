6bfa47b8051f89059483ef587730bd8eeed9eefd
fuck fuck <fuck@fuck.fuck>
2026-08-11 21:45:51 +0900

    adding singbox installation script

---
+++ singbox_install_latest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+vers=$(curl -s https://api.github.com/repos/SagerNet/sing-box/releases |
+  grep tag_name | grep -v beta | head -n 1 | awk -F: '{print $2}' | sed 's/[", v]//g')
+
+echo $vers
+
+curl -v -o pack.deb -L https://github.com/SagerNet/sing-box/releases/download/v$vers/sing-box_''$vers''_linux_amd64.deb
+
+sudo apt install ./pack.deb
