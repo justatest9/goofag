b3ab8a4db7b4216be0da8179605a86933013ecc3
fuck fuck <fuck@fuck.fuck>
2026-08-13 04:03:05 +0900

    moving to ebrasha

--- urlmob.sh
+++ urlmob.sh
@@ -8,10 +8,20 @@ mkdir -p $TMP
 CURPT=$(pwd)
 cd $TMP
 curl -L -O -s https://github.com/igareck/vpn-configs-for-russia/raw/refs/heads/main/Base64/BLACK_VLESS_RUS_base64.txt
+curl -L -o ebrasha1.txt https://raw.githubusercontent.com/ebrasha/free-v2ray-public-list/refs/heads/main/separated-protocols-chunks/vless/EbraSha-Protocol-Chunks-vless-001.txt
+curl -L -o ebrasha2.txt https://raw.githubusercontent.com/ebrasha/free-v2ray-public-list/refs/heads/main/separated-protocols-chunks/vless/EbraSha-Protocol-Chunks-vless-002.txt
+curl -L -o ebrasha3.txt https://raw.githubusercontent.com/ebrasha/free-v2ray-public-list/refs/heads/main/separated-protocols-chunks/vless/EbraSha-Protocol-Chunks-vless-003.txt
+curl -L -o ebrasha4.txt https://raw.githubusercontent.com/ebrasha/free-v2ray-public-list/refs/heads/main/separated-protocols-chunks/vless/EbraSha-Protocol-Chunks-vless-004.txt
 cd $CURPT
 
 base64 -d $TMP/BLACK_VLESS_RUS_base64.txt >$TMP/BLACK_VLESS_RUS_base64_dec.txt
-grep . $TMP/BLACK_VLESS_RUS_base64_dec.txt | grep -iv 'united.*states' | grep -iv russia >$TMP/BLACK_VLESS.txt
+
+rm $TMP/BLACK_VLESS.txt
+grep reality $TMP/ebrasha1.txt >>$TMP/BLACK_VLESS.txt
+grep reality $TMP/ebrasha2.txt >>$TMP/BLACK_VLESS.txt
+grep reality $TMP/ebrasha3.txt >>$TMP/BLACK_VLESS.txt
+grep reality $TMP/ebrasha4.txt >>$TMP/BLACK_VLESS.txt
+#grep . $TMP/BLACK_VLESS_RUS_base64_dec.txt | grep -iv 'united.*states' | grep -iv russia >$TMP/BLACK_VLESS.txt
 #grep reality $TMP/BLACK_VLESS_RUS_base64_dec.txt | grep -iv 'united.*states' | grep -iv russia >$TMP/BLACK_VLESS.txt
 
 echo $PTH
