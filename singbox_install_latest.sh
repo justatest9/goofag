7e828e4c699b21586c299e598d4dda5d03e347a5
fuck fuck <fuck@fuck.fuck>
2026-08-15 00:25:18 +0900

    fix install sing-box - retries added

--- singbox_install_latest.sh
+++ singbox_install_latest.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-vers=$(curl -s https://api.github.com/repos/SagerNet/sing-box/releases |
-  grep tag_name | grep -v beta | head -n 1 | awk -F: '{print $2}' | sed 's/[", v]//g')
+for ((i = 0; i < 3; i++)); do
+  vers=$(curl -s https://api.github.com/repos/SagerNet/sing-box/releases |
+    grep tag_name | grep -v beta | head -n 1 | awk -F: '{print $2}' | sed 's/[", v]//g')
+  if [[ ! -z "$vers" ]]; then break; fi
+done
 
 echo $vers
 
