791ed28884fcc1b59ca2716bfe9acae453b5ce86
fuck fuck <fuck@fuck.fuck>
2026-08-15 11:57:32 +0900

    singbox updater fix (verbosity and +retries)

--- singbox_install_latest.sh
+++ singbox_install_latest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for ((i = 0; i < 3; i++)); do
-  vers=$(curl -s https://api.github.com/repos/SagerNet/sing-box/releases |
+for ((i = 0; i < 10; i++)); do
+  vers=$(curl -v -s https://api.github.com/repos/SagerNet/sing-box/releases |
     grep tag_name | grep -v beta | head -n 1 | awk -F: '{print $2}' | sed 's/[", v]//g')
   if [[ ! -z "$vers" ]]; then break; fi
 done
