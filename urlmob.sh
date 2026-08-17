29d47d196367cb94b55a80a8122ec73c3f07749d
fuck fuck <fuck@fuck.fuck>
2026-08-17 22:49:50 +0900

    fixig urlmod

--- urlmob.sh
+++ urlmob.sh
@@ -20,7 +20,7 @@ grep . $TMP/BLACK_VLESS_RUS_base64_dec.txt | grep -iv 'united.*states' | grep -i
 
 echo $PTH
 #cat $TMP/BLACK_VLESS.txt | python $PTH/make_url_outbounds.py | python append_to_singbox_config.py >$PTH/singbox1.json
-#cat $TMP/TESTED_EBA_HYST_CUT.txt | python $PTH/make_url_outbounds.py | python append_to_singbox_config.py >$PTH/singbox2.json
+cat $TMP/TESTED_EBA_HYST_CUT.txt | python $PTH/make_url_outbounds.py | python append_to_singbox_config.py >$PTH/singbox2.json
 
 # for now just take fixed list and prepare config
 cat exhaust/fixed_list.txt | python $PTH/make_url_outbounds.py | python append_to_singbox_config.py >$PTH/singbox1.json
