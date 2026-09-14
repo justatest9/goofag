dd861b4c1c374d682ce383529d4f01433ad183e3
fuck fuck <fuck@fuck.fuck>
2026-09-15 00:26:19 +0900

    lets try hysteria back due to shitty vless

--- make_tested_list.sh
+++ make_tested_list.sh
@@ -7,11 +7,11 @@ mkdir -p $TMP
 #export https_proxy="192.168.88.1:8080"
 
 (
-  #sh ftch.sh https://raw.githubusercontent.com/ebrasha/free-v2ray-public-list/refs/heads/main/separated-protocols/hysteria2_configs.txt
-  sh ftch.sh https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/BLACK_VLESS_RUS.txt
+  sh ftch.sh https://raw.githubusercontent.com/ebrasha/free-v2ray-public-list/refs/heads/main/separated-protocols/hysteria2_configs.txt
+  #sh ftch.sh https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/BLACK_VLESS_RUS.txt
   #cat $PTH/exhaust/fixed_list.txt
   # ebrasha vless full
-  sh ftch.sh https://raw.githubusercontent.com/ebrasha/free-v2ray-public-list/refs/heads/main/separated-protocols/vless_configs.txt | grep reality
+  #sh ftch.sh https://raw.githubusercontent.com/ebrasha/free-v2ray-public-list/refs/heads/main/separated-protocols/vless_configs.txt | grep reality
 ) |
   python remove_duplicates.py |
   bash $PTH/mttest/test.sh |
